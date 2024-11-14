e078b87dbfe7dd4af9c6abf054207098c34d1327
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-11-14 11:31:53 +0530

    States

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+  const[username, setUsername] = useState("")
+  const handleInput = e =>{
+    setUsername(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1>04-State and Props</h1>
+   <label htmlFor='username'>Enter your name here</label>
+   <input
+   id="username"
+   type='text'
+   placeholder='my wonderful name'
+   onChange={handleInput}
+   />
+    <p>Hi there, {username}</p>
+ 
+   </>
   );
 }
 
